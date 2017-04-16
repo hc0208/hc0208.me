ccfd3d6ca5f1e81f7265aca4e10c8d1e3f0fdb17
hiroki <hiroki22991395@yahoo.co.jp>
2017-04-16 22:39:34 +0900

    update hc0208.github.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,40 @@
 class InsertQiitaItems {
+
+  constructor() {
+    this.execute();
+  }
+
   execute() {
     $.getJSON("http://qiita.com/api/v2/users/hc0208/items", function(res){
       $.each(res, function(i, ele){
-        var date = new Date(ele.created_at)
-        var month = date.getMonth()+1
+
+        var date = new Date(ele.created_at);
+        var month = date.getMonth()+1;
         if (month < 10 ){
-          month = `0${date.getMonth()+1}`
-        }
-        date = `${date.getFullYear()}/${month}/${date.getDate()}`
-        var html = `
+          month = `0${month}`;
+        };
+
+        var tags = []
+        $.each(ele.tags, function(i, ele){
+          tags.push(ele.name);
+        })
+
+        const html = `
           <div class="qiita-collection clearfix">
             <div class="qiita-collection_left">
               <h1><a href="${ele.url}" target="_blank">${ele.title}</a></h1>
+              <ul class="clearfix">
+                <li>${tags.join(', ')}</li>
+              </ul>
             </div>
-            <span class="qiita-collection_right">${date}</span>
+            <span class="qiita-collection_right">
+              ${`${date.getFullYear()}/${month}/${date.getDate()}`}
+            </span>
           </div>
-        `
-        $("#qiitaFeedContent").append(html)
-      })
+        `;
+
+        $("#qiitaFeedContent").append(html);
+      });
     });
   }
 }
